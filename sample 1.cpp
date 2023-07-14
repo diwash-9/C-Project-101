1d177f815929c1fbd6eecaf9f50ae5aa3432006a
ravi <ravi.prince.7374@gmail.com>
2023-07-14 19:00:18 +0545

    uploaded by ravi

--- sample 1.cpp
+++ sample 1.cpp
@@ -19,13 +19,13 @@ int askQuestion(Question q)
     
     // bound to the option only
     A:
-    scanf("%d", &response);
+	scanf("%d", &response);
     if (response > 5 || response < 1){
     	printf("invalid input , please choose from the above option \n ");
     	goto A;
 	}
 
-    printf("------------------------------------------------------\n");
+    printf("------------------------------------------------------\n\n");
     return response;
 }
 
@@ -38,13 +38,16 @@ int adaptWeight(int currentResponse, int previousResponse, int weight)
     }
     return weight;
 }
+	char name[30];
+	char gender[10];
+	int age;
 
 int main()
 {
-    printf("\t-----WELCOME TO THE TEST-----\n\n");
-    printf("\n\t\t***DISCLAIMER***\n\n\t\t*This quiz is NOT a diagnostic tool* \nMental health disorders can only be diagnosed by licensed health care professionals.\n\n\n");
-    printf("\n\t\tWhat Does This Test Consist Of?\n");
-    printf("-> Ten questions that relate to common thoughts and behaviors experienced by those \n\tdiagnosed with obsessive compulsive disorder (OCD).\n\n");
+    printf("\n\n\t\t\t\t\t\t-----WELCOME TO THE TEST-----\n\n");
+    printf("\n\t\t\t\t\t\t\t***DISCLAIMER***\n\n\n\t\t\t\t\t*This quiz is NOT a diagnostic tool* \n\n \t\t\t\t\tMental health disorders can only be diagnosed by licensed health care professionals.\n\n\n");
+    printf("\n\t\t\t\t\tWhat Does This Test Consist Of?\n\n");
+    printf("\t\t\t\t\t-> Ten questions that relate to common thoughts and behaviors experienced by those \n\n\t\t\t\t\tdiagnosed with obsessive compulsive disorder (OCD).\n\n");
 	//for holding and clearing screen
 	system("pause");
 	system("cls");
@@ -53,7 +56,7 @@ int main()
     // Generate unique filename using timestamp
     time_t current_time = time(NULL);
     char filename[100];
-    printf("enter your name =");
+    printf("enter the file name : ");
     scanf("%s",filename);
     sprintf(filename, "%s.doc", filename);
 
@@ -64,8 +67,14 @@ int main()
         printf("Failed to open the file.\n");
         return 1;
     }
-
-
+    
+    fprintf(file,"\n\n\t\t\t\t*****REPORT CARD***** \n\n");
+    fprintf(file,"\n\ntest performed = OCD (obsessive compulsive disorder)");
+    fprintf(file,"\n\nDATE: %s", __DATE__);
+    fprintf(file,"\n\nTIME: %s\n\n", __TIME__);
+    
+    printf("\nlets start-- \n\n");
+    
     Question questions[] = {
         {"Q) I often have repetitive and intrusive thoughts that I find difficult to control.", 2},
         {"Q) I feel a strong urge to perform certain actions or rituals repeatedly.", 3},
@@ -88,27 +97,48 @@ int main()
     for (int i = 0; i < numQuestions; i++)
     {
         int currentResponse = askQuestion(questions[i]);
+        
         questions[i].weight = adaptWeight(currentResponse, previousResponse, questions[i].weight);
+        
         totalScore += currentResponse * questions[i].weight;
+        
         previousResponse = currentResponse;
         
         //report generation
 	
-        fprintf(file, "%s \n Weight = %d, Your Response = %d, Question score = %d\n", questions[i].question, questions[i].weight, currentResponse, currentResponse * questions[i].weight);
-        fprintf(file, "------------------------------------------------------\n");
+//        fprintf(file, "%s \n Weight = %d, Your Response = %d, Question score = %d\n", questions[i].question, questions[i].weight, currentResponse, currentResponse * questions[i].weight);
+//        fprintf(file, "------------------------------------------------------\n");
     }
 	
-	printf("score \n 0-9 --normal \n > 15 --abnoraml \n ");
+	printf("score level: \n 0-15 --normal \n greater than 15 --abnoraml \n\n ");
+	fprintf(file,"\t\tscore level: \n \t\t0-9 --normal \n\t\t> 15 --abnormal \n\n ");
 	
-	
+	printf("your score =  \n\n");
+	fprintf(file,"your score =  \n\n");
     if (totalScore > 15)
     {
-        printf("You may be experiencing symptoms of OCD.\n");
+        printf("You may be experiencing symptoms of OCD.\n\n");
+        printf("\tConsult to a Doctor\n\n");
+        printf("Some advices : \n\n");
+        printf("1.Be Open About OCD\n");
+        printf("2.Talk to someone you trust about your OCD \n");
+        printf("3.Exposure and response prevention (ERP) therapy\n");
+        fprintf(file,"\t\tYou may be experiencing symptoms of OCD.\n\n");
+        fprintf(file,"\t\tConsult to a Doctor\n\n");
+        fprintf(file,"Some advices : \n\n");
+        fprintf(file,"1.Be Open About OCD\n");
+        fprintf(file,"2.Talk to someone you trust about your OCD \n");
+        fprintf(file,"3.Exposure and response prevention (ERP) therapy\n\n\n");
+        
     }
     else
     {
-        printf("You do not seem to display significant symptoms of OCD.\n");
+        printf("\t\tYou do not seem to display significant symptoms of OCD.\n\n");
+        fprintf(file,"\t\tYou do not seem to display significant symptoms of OCD.\n\n");
+       
     }
+    printf("\n\n\t\t\t\tHave a good day");
+    fprintf(file,"\n\n\t\t\t\tHave a good day");
 	
     // Close the file
     fclose(file);
